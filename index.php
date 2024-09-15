efee4cb210cf4eea1a9c791246d26cfcd3d0b046
s4096467 <167684182+ConnorRMIT@users.noreply.github.com>
2024-09-16 09:12:04 +1000

    Created index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Event Organizer</title>
+    
+    <meta charset="UTF-8">
+  </head>
+  <body>
+    <h1>Sample Title</h1>
+  </body>
+</html>

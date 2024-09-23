7b6f4da3e8f4c441469c731eedd478404c17efec
Antipoison7 <orders.connor@gmail.com>
2024-09-23 10:43:19 +1000

    Added 2d arrays to index.php

---
+++ siteRoot/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Event Organizer</title>
+    
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="./Resources/Style/base.css">
+  </head>
+  <body>
+    <h1>Sample Title</h1>
+
+    <div class="creatorFlex">
+      <?php
+        $creatorArray = [
+          array("name" => "Josip", "pfp" => ""), 
+          array("name" => "Declan", "pfp" => ""), 
+          array("name" => "Himanth", "pfp" => ""), 
+          array("name" => "Connor", "pfp" => "connorPfp.jpg")
+        ];
+        foreach($creatorArray as $x)
+        {
+          echo("<div class=\"creatorBox\">
+                <p>" . $x["name"] . "</p>
+                <img src=\"./Resources/Images/Pfp/" . $x["pfp"] . "\" alt=\"" . $x["name"] . " Profile Picture\">
+                </div>");
+        }
+      ?>
+    </div>
+  </body>
+</html>

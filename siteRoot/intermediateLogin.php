32e8a2d49329ad12ef3da998dd05b4da15c9445d
Connor Orders <s4096467@csitprdap02.int.its.rmit.edu.au>
2024-09-30 15:01:14 +1000

    Created intermediate pages for verification

---
+++ siteRoot/intermediateLogin.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<?php
+    session_start();
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+?>
+
+<html lang="en">
+        <head>
+               <title>Please Dont Break</title>
+
+
+        <!-- <meta http-equiv='refresh' content='5'; url ='index.php'/> -->
+        </head>
+
+        <!-- redirectScript() -->
+    <body onload=''>
+
+        <p><a href="index.php">Damn, if you see this and it doesn't load, click this. Do not refresh the page.</a></p>
+        <?php
+            echo(var_dump($_POST));
+        ?>
+            <script>
+                // function redirectScript()
+                // {
+                //     sleep(1000);
+                //     window.location.replace("index.php");
+                // }
+                // function sleep(ms) {
+                //     return new Promise(resolve => setTimeout(resolve, ms));
+                // }
+            </script>
+
+    </body>
+</html>

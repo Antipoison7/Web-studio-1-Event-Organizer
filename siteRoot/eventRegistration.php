2dfd24371a8a72593d8677809f11d16159dade47
Declan Thomas <s4009378@csitprdap01.int.its.rmit.edu.au>
2024-10-04 10:35:00 +1000

    updating headers and footers with new style

--- siteRoot/eventRegistration.php
+++ siteRoot/eventRegistration.php
@@ -9,16 +9,22 @@
     <meta charset="UTF-8">
     <link rel="stylesheet" href="./Resources/Style/base.css">
     <link rel="icon" type="image/x-icon" href="./Resources/Images/Resources/favicon.png">
+
   </head>
-  <body>
-    <?php headerNoLogin("Event Registraion") ?>
-      <form>
-        <label for="etitle">Whats the title of your event?</label><br>
-        <input type="text" id="eventTitle" name="eventTitle"><br>
-        <label for="eventDescription">Whats your event about?</label><br>
-        <input type="text" id="eventDescription" name="eventDescription"><br>
-        <label for="eimg">What image would you like displayed? </label><br>
-        <input type="file" id="eimg" name="eimg" accept="image/*\">
+  <body style="background-color: lightgray;">
+    <?php headerNoLogin("Event Registration") ?>
+      <form class="regBlock">
+        <div>
+
+          <label for="etitle">Whats the title of your event?</label><br>
+            <input type="text" id="eventTitle" name="eventTitle"><br>
+        
+          <label for="eventDescription">Whats your event about?</label><br>
+            <input type="text" id="eventDescription" name="eventDescription"><br>
+        
+          <label for="eimg">What image would you like displayed? </label><br>
+            <input type="file" id="eimg" name="eimg" accept="image/*\">
+        </div>
       </form>
     
     <?php

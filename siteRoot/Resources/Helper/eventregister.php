5507e32d443b8745d538d8308a541cf157d20d7a
Declan Thomas <s4009378@csitprdap01.int.its.rmit.edu.au>
2024-10-07 10:10:55 +1100

    added function for adding events

--- siteRoot/Resources/Helper/eventregister.php
+++ siteRoot/Resources/Helper/eventregister.php
@@ -4,12 +4,13 @@ $username = "COSC3046_2402_UGRD_1479_G4";
 $password = "GYS3sfUkzIqA";
 $dbname = "COSC3046_2402_UGRD_1479_G4";
 
+function createEvent( $name, $desc, $url, $link, $region ) {
 try {
   $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
   
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   $sql = "INSERT INTO EventList (eventName, eventDescription, priceURL, imageLink, Region)
-  VALUES ()";
+  VALUES ($name, $desc, $url, $link, $region)";
   
   $conn->exec($sql);
   echo "New record created successfully";
@@ -18,4 +19,5 @@ try {
 }
 
 $conn = null;
+}
 ?>

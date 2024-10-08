34aa72a10c12b4e1d9328d9a38104d5565b002e2
Declan Thomas <s4009378@csitprdap01.int.its.rmit.edu.au>
2024-10-09 10:41:25 +1100

    fixing discussion forum

--- siteRoot/discussionForum.php
+++ siteRoot/discussionForum.php
@@ -15,49 +15,34 @@
   <body>
     <?php headerNoLogin("Discussion Board") ?>
     
-    <?php
-        echo "<div class='discussionBoard'>";
-        echo "<tr><th>Id</th><th>Firstname</th><th>Lastname</th></tr>";
+        <?php
+            $servername = "talsprddb02.int.its.rmit.edu.au:3306";
+            $username = "COSC3046_2402_UGRD_1479_G4";
+            $password = "GYS3sfUkzIqA";
+            $dbname = "COSC3046_2402_UGRD_1479_G4";
 
-        class TableRows extends RecursiveIteratorIterator {
-          function __construct($it) {
-            parent::__construct($it, self::LEAVES_ONLY);
-          }
+                  
+            $conn = mysqli_connect($servername, $username, $password, $dbname);
+                  
+            if (!$conn) {
+              die("Connection failed: " . mysqli_connect_error());
+            }
 
-        function current() {
-            return "<div class='DiscussionPost'>" . parent::current(). "</div>";
-        }
+            $sql = "SELECT eventName, eventDesc, priceCost, imageLink, Region FROM EventList";
+            $result = mysqli_query($conn, $sql);
+            echo "<div class='DiscussionBlock'>";
 
-        function beginChildren() {
-            echo "<br>";
-        }
+            if (mysqli_num_rows($result) > 0) {
+                    
+              while($row = mysqli_fetch_assoc($result)) {
+                echo "<div class='DiscussionPost'>" . $row["eventName"]. " " . $row["eventDesc"]. " " . $row["priceCost"]. "<br>" . $row["imageLink"]. " " . $row["Region"]. "</div>";
+              }
+            } else {
+              echo "0 results";
+            }
+            echo "</div>";
 
-        function endChildren() {
-            echo "<br>" . "\n";
-        }
-        }
-
-        $servername = "talsprddb02.int.its.rmit.edu.au:3306";
-        $username = "COSC3046_2402_UGRD_1479_G4";
-        $password = "GYS3sfUkzIqA";
-        $dbname = "COSC3046_2402_UGRD_1479_G4";
-
-        try {
-        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $stmt = $conn->prepare("SELECT eventName, eventDesc, priceURL, imageLink, Region FROM EventList");
-        $stmt->execute();
-
-        // set the resulting array to associative
-        $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
-        foreach(new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) {
-            echo $v;
-        }
-        } catch(PDOException $e) {
-        echo "Error: " . $e->getMessage();
-        }
-        $conn = null;
-        echo "</div>";
+            mysqli_close($conn);
         ?>
 
 

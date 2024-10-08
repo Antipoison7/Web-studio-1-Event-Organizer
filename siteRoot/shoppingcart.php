9a82f3b641c162342cc64cc9f185aefd605e6712
Josip Kasic <s4094394@csitprdap01.int.its.rmit.edu.au>
2024-10-09 08:52:40 +1100

    basic html to shopping cart page

---
+++ siteRoot/shoppingcart.php
@@ -0,0 +1,24 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Shopping Cart</title>
+</head>
+<body>
+    <header>
+        <h1>Shopping Cart</h1>
+    </header>
+    
+    <main>
+    </main>
+
+    <footer>
+        <div>Footer Content</div>
+    </footer>
+</body>
+</html>

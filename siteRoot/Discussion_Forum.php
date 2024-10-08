9b14ce05d3d80981884bfecd1eb621051402b16f
Declan Thomas <s4009378@csitprdap01.int.its.rmit.edu.au>
2024-10-09 08:51:35 +1100

    events now store in sql table, created discussion board

---
+++ siteRoot/Discussion_Forum.php
@@ -0,0 +1,28 @@
+<?php
+  include_once('./Resources/Helper/headers.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Discussion Forum</title>
+    
+    
+    <?php createMeta() ?>
+    <link rel="stylesheet" href="./Resources/Style/base.css">
+    <link rel="icon" type="image/x-icon" href="./Resources/Images/Resources/favicon.png">
+  </head>
+  <body>
+    <?php headerNoLogin("Discussion Board") ?>
+    <div class="discussionBoard">
+
+
+
+        
+
+      
+    </div>
+    <?php
+      makeFooter();
+    ?>
+  </body>
+</html>
--- siteRoot/Resources/Helper/eventregister.php
+++ siteRoot/Resources/Helper/eventregister.php
@@ -10,18 +10,38 @@ try {
   
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-        $event_name =  $_REQUEST['eventTitle'];
-        $event_desc = $_REQUEST['eventDescription'];
-        $event_link =  $_REQUEST['eLink'];
-        $event_region = $_REQUEST['eventRegion'];
-        $event_image = $_REQUEST['eimg'];
+  If($_POST){       
+              $event_name =  $_REQUEST['eventTitle'];
+              $event_desc = $_REQUEST['eventDescription'];
+              $event_link =  $_REQUEST['eLink'];
+              $event_region = $_REQUEST['eventRegion'];
+              $event_image = $_REQUEST['eimg'];
+    
+    
+
+            $stmt = $conn->prepare("INSERT INTO EventList (eventName, eventDesc, priceURL, imageLink, Region) VALUES (:name, :description, :linkTo, :img, :Region);");
+
+            $stmt->bindParam(':name', $event_name, PDO::PARAM_STR);
+            $stmt->bindParam(':description', $event_desc, PDO::PARAM_STR);
+            $stmt->bindParam(':linkTo', $event_link, PDO::PARAM_STR);
+            $stmt->bindParam(':Region', $event_region, PDO::PARAM_STR);
+            $stmt->bindParam(':img', $event_image, PDO::PARAM_STR);
+
+            $stmt->execute();
+    
+              
 
 
-  $sql = "INSERT INTO EventList (eventName, eventDescription, priceURL, imageLink, Region)
-  VALUES ($event_name, $event_desc, $event_link, $event_image, $event_region)";
+              //$sql = "INSERT INTO EventList (eventName, eventDescription, priceURL, imageLink, Region)
+              //VALUES ($event_name, $event_desc, $event_link, $event_image, $event_region)";
+
+              //$conn->exec($sql);
+              echo "New record created successfully";
+            }
+
+
   
-  $conn->exec($sql);
-  echo "New record created successfully";
+  
 } catch(PDOException $e) {
   echo $sql . "<br>" . $e->getMessage();
 

9ffd7597e0231863def019199eca34059420c23c
ConnorRMIT <s4096467@student.rmit.edu.au>
2024-10-09 11:10:29 +1100

    Created User customisation page css

--- siteRoot/profileCustomise.php
+++ siteRoot/profileCustomise.php
@@ -12,32 +12,14 @@
     <link rel="icon" type="image/x-icon" href="./Resources/Images/Resources/favicon.png">
   </head>
   <body>
-    <div class="loginHeader">
+    <div class="profileHeader">
         <a href="./">
             <div class="button">Home</div>
         </a>
-        <h1>User Login</h1>
+        <h1>Customise profile</h1>
         <div style="width: 14em; height: 3em;"></div>
     </div>
 
-    <form method="post" action="./intermediateLogin.php">
-      <div class="login">
-        <label for="username">Username / Email:</label>
-        <input type="text" id="username" name="username" autocomplete="username">
-
-        <label for="password">Password: <a href="./forgot">Forgot password</a></label>
-        <input type="password" id="password" name="password">
-
-        <!-- Sets the value passed to the intermediate to Register -->
-        <input type="text" id="hiddenType" name="hiddenType" value="Login" hidden>
-
-        <div class="flex">
-          <button type="submit" class="smallButton" style="margin-right: 5px;">Login</button>
-          <a href="./register.php"><div class="smallButtonInv">Register</div></a>
-          
-        </div>
-      </div>
-    </form>
 
     <?php makeFooter(); ?>
   </body>

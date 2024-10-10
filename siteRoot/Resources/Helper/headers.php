29d97acb4bcc29ae091c8b859e44d12d4b61e6ad
Declan Thomas <s4009378@csitprdap01.int.its.rmit.edu.au>
2024-10-11 09:36:13 +1100

    updated header button to now use the hover feature

--- siteRoot/Resources/Helper/headers.php
+++ siteRoot/Resources/Helper/headers.php
@@ -61,7 +61,7 @@
                 <div class=\"mainHeader\">
                     <h1>$title</h1>
                     <div>
-                        <a href=\"./login.php\"><div class=\"smallButton\">Login</div></a>
+                        <a href=\"./login.php\"><div class=\"smallButtonInv\">Login</div></a>
                         <a href=\"./register.php\"><div class=\"smallButtonInv\">Register</div></a>
                         <a href=\"./shoppingcart.php\" class=\"cartIcon\"><img src=\"./Resources/Images/Resources/shoppingCart.svg\" alt=\"Your Cart\" style=\"height:3em\"></a></div>
                     </div>

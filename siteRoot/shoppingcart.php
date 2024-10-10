6b6cee4bfec651a96ec72808f5660983facf225e
Josip Kasic <s4094394@csitprdap01.int.its.rmit.edu.au>
2024-10-11 09:32:57 +1100

    shopping cart

--- siteRoot/shoppingcart.php
+++ siteRoot/shoppingcart.php
@@ -58,27 +58,3 @@
     <?php makeFooter(); ?>
 
 
-   
-<?php
-session_start();
-include_once('./Resources/Helper/headers.php');
-?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <link rel="stylesheet" href="./Resources/Style/base.css">
-    <link rel="icon" type="image/x-icon" href="./Resources/Images/Resources/favicon.png">
-    <title>Shopping Cart</title>
-
-</head>
-<body>
-    <?php headerNoLogin("Shopping Cart"); ?>
-    
-    <main>
-    </main>
-
-    <?php makeFooter(); ?>
-</body>
-</html>

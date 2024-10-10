41e05b242000f0a147d46bd53dab880c8d5a0af2
ConnorRMIT <s4096467@student.rmit.edu.au>
2024-10-11 10:01:37 +1100

    Added dynamic fill to pfpCustomise

--- siteRoot/profileCustomise.php
+++ siteRoot/profileCustomise.php
@@ -1,6 +1,7 @@
 <?php
   include_once('./Resources/Helper/headers.php');
   include_once('./Resources/Helper/loginHelper.php');
+  include_once('./Resources/Helper/userDetailsHelper.php');
   session_start();
 ?>
 <!DOCTYPE html>
@@ -25,22 +26,28 @@
     <?php if(isset($_SESSION["loginDetails"])){if(isValidLogin($_SESSION["loginDetails"]["username"], $_SESSION["loginDetails"]["password"])){?>
       <div class="content">
         <form class="uncrunch">
-          <div>
-            <input type="file" name="pfp" id="pfp">
-            <div>
+          <div class="flex userInfoContainer">
+            <label for="pfp"><img class="customisePfp" src=".<?php echo(getProfilePicture($_SESSION["loginDetails"]["username"])); ?>"></label>
+            <input type="file" name="pfp" id="pfp" hidden>
+            <div class="userInfoBoxR">
               <label for="displayName">Display Name</label>
-              <input type="text" name="displayName" id="displayName">
-              <label for="realName">Real Name</label>
-              <input type="text" name="realName" id="realName">
+              <input type="text" name="displayName" id="displayName" value="<?php echo(getDisplayName($_SESSION["loginDetails"]["username"])) ?>">
+              <label for="realName" >Real Name</label>
+              <input type="text" name="realName" id="realName" value="<?php echo(getRealName($_SESSION["loginDetails"]["username"])) ?>">
             </div>
           </div>
-          <label for="description">Description</label>
-          <textarea id="description" name="description" rows="4" cols="50">
-          </textarea>
+          <div class="userInfoContainer userInfoBoxR">
+            <label for="description">Description</label>
+            <textarea id="description" name="description" rows="4" cols="50" value="<?php echo(getRealName($_SESSION["loginDetails"]["username"])) ?>">
+            </textarea>
+          </div>
 
-          <!-- <div class="split"></div> -->
+          <div class="split"></div>
 
-          <label for="theme">Profile theme: <a>Custom Themes?</a></label>
+          <div class="flex">
+          <label for="theme">Profile theme:</label>
+          <a href="https://www.google.com">Custom Themes?</a>
+          </div>
           <select name="theme" id="theme">
             <option value="lightMode.css">Light mode</option>
             <option value="darkMode.css">Dark mode</option>

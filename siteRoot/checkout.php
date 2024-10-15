bf231caeff3b79fdf87cb9f82d5803958e4ff517
Josip Kasic <s4094394@csitprdap01.int.its.rmit.edu.au>
2024-10-16 10:47:12 +1100

    checkout

--- siteRoot/checkout.php
+++ siteRoot/checkout.php
@@ -10,7 +10,6 @@ include_once('./Resources/Helper/headers.php');
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./Resources/Style/checkoutpage.css">
     <link rel="stylesheet" href="./Resources/Style/base.css">
-    <link rel="icon" type="image/x-icon" href="./Resources/Images/Resources/favicon.png">
     <title>Checkout</title>
 </head>
 <body>

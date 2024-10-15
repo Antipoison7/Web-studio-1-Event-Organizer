83559b0dbbe7ab58257709bce88165cb6e8fd6d9
Declan Thomas <s4009378@csitprdap01.int.its.rmit.edu.au>
2024-10-16 09:48:37 +1100

    updated index.php

--- siteRoot/index.php
+++ siteRoot/index.php
@@ -12,23 +12,21 @@
     <link rel="icon" type="image/x-icon" href="./Resources/Images/Resources/favicon.png">
   </head>
   <body>
-    <?php headerNoLogin("Meet the Creators") ?>
-    <div class="creatorFlex">
-      <?php
-        $creatorArray = [
-          array("name" => "Josip", "pfp" => ""), 
-          array("name" => "Declan", "pfp" => "declanPfp.jpg"), 
-          array("name" => "Himanth", "pfp" => "rock.jpeg"), 
-          array("name" => "Connor", "pfp" => "connorPfp.jpg")
-        ];
-        foreach($creatorArray as $x)
-        {
-          echo("<div class=\"creatorBox\">
-                <p>" . $x["name"] . "</p>
-                <img src=\"./Resources/Images/userPfp/" . $x["pfp"] . "\" alt=\"" . $x["name"] . " Profile Picture\">
-                </div>");
-        }
-      ?>
+  <?php createMeta() ?>
+    <link rel="stylesheet" href="./Resources/Style/base.css">
+    <link rel="icon" type="image/x-icon" href="./Resources/Images/Resources/favicon.png">
+  </head>
+  <body>
+    <?php headerNoLogin("Home Page") ?>
+    <div class="BannerFlex">
+      <br>
+      <img id="WebsiteBanner" src="./Resources/Images/Resources/Website banner_Final.jpg" alt="WebsiteBanner" width=100%>
+    </div>
+    <div class="HomePageFlex">
+      <a href="./register.php"><div class="smallButtonInv">Register</div></a>
+      <a href="./eventRegistration.php"><div class="smallButtonHomePage">Create Event</div></a>
+
+    </div>
     </div>
     <?php
       makeFooter();

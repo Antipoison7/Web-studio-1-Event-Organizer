dc075740efc36df2237400cc81a62662d1986809
Declan Thomas <s4009378@csitprdap01.int.its.rmit.edu.au>
2024-10-16 09:00:55 +1100

    editing popup message

--- siteRoot/discussionForum.php
+++ siteRoot/discussionForum.php
@@ -35,7 +35,7 @@
             if (mysqli_num_rows($result) > 0) {
                     
               while($row = mysqli_fetch_assoc($result)) {
-                echo "<div class='DiscussionPost'><div id='discussionpost_layout'><div id='Event_Title'><h1>" . $row["eventName"]. "</h1></div><div id='Event_Description'> " . $row["eventDesc"]. "</div><div id='Event_Image'> <img id=\"discussionImage\" src=\"./Resources/Images/Resources/day in the park example.jpg\" alt=\"day in the park image\"></div><element id='Price_Amount'>$<a href=\"#modal\">" . $row["priceCost"]. "</a></div></div><div id=\"popup-ticket\">Ticket added to Kart<a href=\"#\">Close the Modal</a></div>";
+                echo "<div class='DiscussionPost'><div id='discussionpost_layout'><div id='Event_Title'><h1>" . $row["eventName"]. "</h1></div><div id='Event_Description'> " . $row["eventDesc"]. "</div><div id='Event_Image'> <img id=\"discussionImage\" src=\"./Resources/Images/Resources/day in the park example.jpg\" alt=\"day in the park image\"></div><element id='Price_Amount'>$<a href=\"#popup-ticket\">" . $row["priceCost"]. "</a></div></div><div id=\"popup-ticket\">Ticket added to Cart <a href=\"#\"> Close the Popup</a></div>";
               }
             } else {
               echo "0 results";

b1fed24549a2722354b40eb07def5e3837eefb9f
ConnorRMIT <s4096467@student.rmit.edu.au>
2024-10-20 20:55:39 +1100

    Added admin skeleton

--- siteRoot/adminControls.php
+++ siteRoot/adminControls.php
@@ -1,6 +1,11 @@
 <?php
   session_start();
   include_once('./Resources/Helper/headers.php');
+  include_once('./Resources/Helper/loginHelper.php');
+  if(isset($_SESSION["loginDetails"]))
+  {
+      $isAdmin = isValidAdminLogin($_SESSION["loginDetails"]["username"], $_SESSION["loginDetails"]["password"]);
+  }
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -13,21 +18,31 @@
     <link rel="icon" type="image/x-icon" href="./Resources/Images/Resources/favicon.png">
   </head>
   <body>
-    <?php headerNoLogin("Moderator Portal") ?>
+    <?php headerNoLogin("Moderator Portal"); 
+     if($isAdmin == true){?>
         <div>
             <h2>User management</h2>
             <ul>
-                <li><a href="./">Archive Accounts</a></li>
-                <li><a href="./">Reset Cooldowns</a></li>
-                <li><a href="./">Set Cooldowns</a></li>
+                <li><a href="./Admin/archiveAccount.php">Archive Accounts</a></li>
+                <li><a href="./Admin/resetCooldown.php">Reset Cooldowns</a></li>
+                <li><a href="./Admin/setCooldowns.php">Set Cooldowns</a></li>
             </ul>
             <h2>Post management</h2>
             <ul>
-                <li><a href="./">Unarchive Posts</a></li>
-                <li><a href="./">Unarchive Events</a></li>
+                <li><a href="./Admin/unarchivePosts.php">Unarchive Posts</a></li>
+                <li><a href="./Admin/unarchiveEvents.php">Unarchive Events</a></li>
             </ul>
         </div>
     <?php
+     }
+     else
+     {?>
+      <div class="failedLogin">
+      <h1>You need to log in / have insufficient clearance</h2>
+      <h2><a href="./login.php">Log in with an admin account</a></h2>
+    </div>
+    <?php
+     }
       makeFooter();
     ?>
   </body>

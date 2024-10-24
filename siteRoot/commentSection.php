43d96f6240d7a3c698c88d78acb4f433a095d0a4
Declan Thomas <s4009378@csitprdap01.int.its.rmit.edu.au>
2024-10-25 10:20:47 +1100

    adding javascript

--- siteRoot/commentSection.php
+++ siteRoot/commentSection.php
@@ -30,8 +30,8 @@
       <div class="container">
         <div class="row">
                 <div class="col-md-6 col-md-offset-3 post">
-                        <h2><?php echo $post['title'] ?></h2>
-                        <p><?php echo $post['body']; ?></p>
+                        <h2><?php echo $post['eventName'] ?></h2>
+                        <p><?php echo $post['eventDesc']; ?></p>
                 </div>
                 <div class="col-md-6 col-md-offset-3 comments-section">
                         
@@ -100,6 +100,72 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+
+
+<script type="text/javascript">
+  $(document).ready(function(){
+        // When user clicks on submit comment to add comment under post
+        $(document).on('click', '#submit_comment', function(e) {
+                e.preventDefault();
+                var comment_text = $('#comment_text').val();
+                var url = $('#comment_form').attr('action');
+                // Stop executing if not value is entered
+                if (comment_text === "" ) return;
+                $.ajax({
+                        url: url,
+                        type: "POST",
+                        data: {
+                                comment_text: comment_text,
+                                comment_posted: 1
+                        },
+                        success: function(data){
+                                var response = JSON.parse(data);
+                                if (data === "error") {
+                                        alert('There was an error adding comment. Please try again');
+                                } else {
+                                        $('#comments-wrapper').prepend(response.comment)
+                                        $('#comments_count').text(response.comments_count); 
+                                        $('#comment_text').val('');
+                                }
+                        }
+                });
+        });
+        // When user clicks on submit reply to add reply under comment
+        $(document).on('click', '.reply-btn', function(e){
+                e.preventDefault();
+                // Get the comment id from the reply button's data-id attribute
+                var comment_id = $(this).data('id');
+                // show/hide the appropriate reply form (from the reply-btn (this), go to the parent element (comment-details)
+                // and then its siblings which is a form element with id comment_reply_form_ + comment_id)
+                $(this).parent().siblings('form#comment_reply_form_' + comment_id).toggle(500);
+                $(document).on('click', '.submit-reply', function(e){
+                        e.preventDefault();
+                        // elements
+                        var reply_textarea = $(this).siblings('textarea'); // reply textarea element
+                        var reply_text = $(this).siblings('textarea').val();
+                        var url = $(this).parent().attr('action');
+                        $.ajax({
+                                url: url,
+                                type: "POST",
+                                data: {
+                                        comment_id: comment_id,
+                                        reply_text: reply_text,
+                                        reply_posted: 1
+                                },
+                                success: function(data){
+                                        if (data === "error") {
+                                                alert('There was an error adding reply. Please try again');
+                                        } else {
+                                                $('.replies_wrapper_' + comment_id).append(data);
+                                                reply_textarea.val('');
+                                        }
+                                }
+                        });
+                });
+        });
+});
+</script>
     </div>
     <?php
       makeFooter();

99802cfc3a0a0b0717d01ba5ddff630e32917b6c
Josip Kasic <s4094394@csitprdap01.int.its.rmit.edu.au>
2024-10-30 09:35:58 +1100

    coupon

--- siteRoot/apply_coupon.php
+++ siteRoot/apply_coupon.php
@@ -2,6 +2,7 @@
 session_start();
 include_once('./Resources/Helper/headers.php');
 
+// Database connection details
 $servername = "talsprddb02.int.its.rmit.edu.au";
 $username = "COSC3046_2402_UGRD_1479_G4";
 $password = "GYS3sfUkzIqA";
@@ -13,10 +14,12 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
+// Get coupon code from AJAX request
 $data = json_decode(file_get_contents("php://input"), true);
 $couponCode = $conn->real_escape_string($data['couponCode']);
 $response = ['success' => false];
 
+// Check if coupon code exists and is valid
 if ($couponCode) {
     $query = "SELECT discount FROM coupons WHERE code = '$couponCode' AND expires_at > NOW()";
     $result = $conn->query($query);

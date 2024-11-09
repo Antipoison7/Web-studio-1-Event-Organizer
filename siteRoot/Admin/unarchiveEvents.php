61164e4fafcc3b02da386641fc1957736448b110
ConnorRMIT <s4096467@student.rmit.edu.au>
2024-11-09 17:51:59 +1100

    Added unarchiving Events

--- siteRoot/Admin/unarchiveEvents.php
+++ siteRoot/Admin/unarchiveEvents.php
@@ -10,13 +10,52 @@
   {
     $isAdmin = false;
   }
+
+  function makeUserResults()
+  {
+    $db = new PDO("mysql:host=talsprddb02.int.its.rmit.edu.au;dbname=COSC3046_2402_UGRD_1479_G4", "COSC3046_2402_UGRD_1479_G4", "GYS3sfUkzIqA");
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        $stmt = $db->prepare("SELECT eventID, eventName, eventDesc, priceCost, imageLink, Region, archived FROM EventList WHERE Archived = 1;");
+        $stmt->execute();
+
+        $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        $db = null;
+        $stmt = null;
+
+        foreach($result as $x)
+        {
+
+          if(strLen($x["imageLink"]) <= 0)
+          {
+            $imgName = "../Resources/Images/Events/thumbnails/day in the park example.jpg";
+          }
+          else
+          {
+            $imgName = "../Resources/Images/Events/" . $x["imageLink"];
+          }
+
+    echo("<input type=\"radio\" name=\"deleteRadio\" id=\"event" . $x["eventID"] . "\" value=\"" . $x["eventID"] . "\" class=\"hideRadioButton\">
+        <label for=\"event" . $x["eventID"] . "\" class=\"userBox\">
+          <div class=\"highlightBox\">
+            <img src=\"" . $imgName . "\">
+            <h3>ID: " . htmlspecialchars($x["eventID"]) . " | " . htmlspecialchars($x["eventName"]) . "</h3>
+            <h4>Description: " . htmlspecialchars($x["eventDesc"]) . "</h4>
+            <h4>Cost: " . htmlspecialchars($x["priceCost"]) . "</h4>
+            <p>Region: " . htmlspecialchars($x["Region"]) . "</p>
+          </div>
+        </label>");
+        }
+  }
 ?>
 <!DOCTYPE html>
 <html lang="en">
   <head>
     <title>Moderator Portal</title>
     
-    
+    <span style="word-break: keep-all;"></span>
+
     <?php createMeta() ?>
     <link rel="stylesheet" href="../Resources/Style/base.css">
     <link rel="stylesheet" href="../Resources/Style/admin.css">
@@ -26,7 +65,35 @@
     <?php headerNoLogin("Moderator Portal - Unarchive Events"); 
      if($isAdmin == true){?>
         <div class="admin">
-            <h2><a class="back" href="../adminControls.php">Back</a></h2>
+          <form action="./unarchiveEventsIntermediate.php" method="post">
+            <div class="submitHeaderDiv">
+              <h2><a class="back" href="../adminControls.php">Back</a></h2>
+              <button class="button" type="submit" style="width: 13em;">Unarchive Event</button>
+            </div>
+            <?php
+              if(isset($_SESSION["issues"]["adminStuff"]))
+              {
+                if($_SESSION["issues"]["adminStuff"] == "Changed")
+                {
+                  echo("<h2>Post Restored</h2>");
+                }
+                if($_SESSION["issues"]["adminStuff"] == "What happened?")
+                {
+                  echo("<h2 class=\"darkRedColor\">Post Not Found</h2>");
+                }
+                if($_SESSION["issues"]["adminStuff"] == "NotSet")
+                {
+                  echo("<h2 class=\"darkRedColor\">Pick a post please</h2>");
+                }
+              }
+              
+            ?>
+              <div class="userDetails">
+                <?php
+                  makeUserResults();
+                ?>
+              </div>
+            </form>
         </div>
     <?php
      }
@@ -41,4 +108,26 @@
       makeFooter();
     ?>
   </body>
+
+  <script>
+        let element = document.getElementsByClassName("hideRadioButton");
+
+        for (i of element)
+        {
+            i.addEventListener("click", highlightSelected);
+        }
+
+        function highlightSelected(event)
+        {
+            let restOf = document.getElementsByClassName("highlightBox");
+            for (i of restOf)
+            {
+                i.style.boxShadow = "0px 0px 0px 3px #000000";
+            }
+            console.log(this.nextSibling.nextSibling.childNodes[0].nextElementSibling.style.boxShadow = "0px 0px 13px 2px #00E06C");
+        }
+    </script>
 </html>
+<?php
+$_SESSION["issues"]["adminStuff"] = null;
+?>
